f3bc7742f690c0d24b3369d2d540f34114c6c35f
Xiaoxiao-sc <104687346+Xiaoxiao-sc@users.noreply.github.com>
2022-05-08 17:30:13 -0400

    Update noise_model.py

--- noise_model.py
+++ noise_model.py
@@ -5,24 +5,10 @@ import numpy as np
 import cv2
 
 
-def get_noise_model(noise_type="gaussian,0,50"):
+def get_noise_model(noise_type="text,0,50"):
     tokens = noise_type.split(sep=",")
 
-    if tokens[0] == "gaussian":
-        min_stddev = int(tokens[1])
-        max_stddev = int(tokens[2])
-
-        def gaussian_noise(img):
-            noise_img = img.astype(np.float)
-            stddev = np.random.uniform(min_stddev, max_stddev)
-            noise = np.random.randn(*img.shape) * stddev
-            noise_img += noise
-            noise_img = np.clip(noise_img, 0, 255).astype(np.uint8)
-            return noise_img
-        return gaussian_noise
-    elif tokens[0] == "clean":
-        return lambda img: img
-    elif tokens[0] == "text":
+    if tokens[0] == "text":
         min_occupancy = int(tokens[1])
         max_occupancy = int(tokens[2])
 
@@ -49,19 +35,8 @@ def get_noise_model(noise_type="gaussian,0,50"):
                     break
             return img
         return add_text
-    elif tokens[0] == "impulse":
-        min_occupancy = int(tokens[1])
-        max_occupancy = int(tokens[2])
-
-        def add_impulse_noise(img):
-            occupancy = np.random.uniform(min_occupancy, max_occupancy)
-            mask = np.random.binomial(size=img.shape, n=1, p=occupancy / 100)
-            noise = np.random.randint(256, size=img.shape)
-            img = img * (1 - mask) + noise * mask
-            return img.astype(np.uint8)
-        return add_impulse_noise
     else:
-        raise ValueError("noise_type should be 'gaussian', 'clean', 'text', or 'impulse'")
+        raise ValueError("noise_type should be 'text'")
 
 
 def get_args():
@@ -69,7 +44,7 @@ def get_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--image_size", type=int, default=256,
                         help="training patch size")
-    parser.add_argument("--noise_model", type=str, default="gaussian,0,50",
+    parser.add_argument("--noise_model", type=str, default="text,0,50",
                         help="noise model to be tested")
     args = parser.parse_args()
     return args
